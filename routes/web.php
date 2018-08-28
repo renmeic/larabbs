3275f3a7af14b54b8cb71fbaa2b5d675c3c9da86
zhr <renmeic@163.com>
2018-08-28 02:25:05 +0000

    category topic list

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
