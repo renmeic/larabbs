1f09686c63d048131e62e2131333b1752741b4ce
zhr <renmeic@163.com>
2018-09-02 14:00:15 +0000

    integrate laravel-permission package

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -36,6 +36,10 @@ class UsersTableSeeder extends Seeder
         $user->name = 'zhr';
         $user->email = 'zhr@zhr.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->assignRole('Founder');
         $user->save();
+
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
