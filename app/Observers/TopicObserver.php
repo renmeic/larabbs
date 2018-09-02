2edfe15d72cfc9b30989a49a15189c69aaa218e2
zhr <renmeic@163.com>
2018-09-02 07:08:11 +0000

    auto translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -10,8 +11,14 @@ use App\Models\Topic;
 class TopicObserver
 {
     public function saving(Topic $topic)
-    {
+    {	
+    	// XSS 过滤
     	$topic->body = clean($topic->body, 'default');
+
         $topic->excerpt = make_excerpt($topic->body);
+
+        if(!$topic->slug) {
+        	$topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
