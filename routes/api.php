22f32b18dff541aef0d8fb266acf78d6748b1989
zhr <renmeic@163.com>
2018-09-03 08:46:19 +0000

    social authorizations

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+	'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+	// 第三方登录
+	$api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+	    ->name('api.socials.authorizations.store');
 });
