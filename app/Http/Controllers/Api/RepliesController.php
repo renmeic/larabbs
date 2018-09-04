8ca19918353b573f86bb2c3b06f52b94b8ead6eb
zhr <renmeic@163.com>
2018-09-04 02:35:16 +0000

    replies store

---
+++ app/Http/Controllers/Api/RepliesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Transformers\ReplyTransformer;
+use App\Http\Requests\Api\ReplyRequest;
+use App\Models\Topic;
+use App\Models\User;
+use App\Models\Reply;
+
+class RepliesController extends Controller
+{
+    public function store(ReplyRequest $request, Topic $topic, Reply $reply)
+    {
+    	$reply->content = $request->content;
+        $reply->topic_id = $topic->id;
+        $reply->user_id = $this->user()->id;
+        $reply->save();
+
+    	return $this->response->item($reply, new ReplyTransformer())->setStatusCode(201);
+    }
+}
